e3acdd704a80ed72a89b7bf1e8b927b087035b0c
Antonio Dolce <dolce.antonio@gmail.com>
2020-06-03 22:59:24 +0200

    update terraform version

--- ecs-cluster/main.tf
+++ ecs-cluster/main.tf
@@ -1,7 +1,7 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
-  required_version = "= 0.12.17"
+  required_version = "= 0.12.24"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
